e6c593b148c1ad11e367a652b65d5be806f14717
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-08-25 01:08:03 +0200

    Add files via upload

---
+++ ASP NET MVC/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaAppHomework.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaAppHomework.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("razor/{id}")]
+        public IActionResult RazorOrder(int? id)
+        {
+            var person = new User
+            {
+                Address = "Street 01",
+                FirstName = "Vasko",
+                LastName = "Vasko",
+                Phone = 1234567
+            };
+            var order = new Order
+            {
+                Id = 10,
+                Pizza = "Kapri",
+                Price = 330,
+                User = person,
+                Delivered = false
+            };
+            if (id.HasValue)
+            {
+                return View(order);
+
+            }
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ ASP NET MVC/Models/Domain/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaAppHomework.Models.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public double Price { get; set; }
+        public string Pizza { get; set; }
+        public bool Delivered { get; set; }
+
+
+        public string GetPriceRange()
+        {
+            if (Price > 1000)
+            {
+                return "$$$$";
+            }
+            else if (Price > 500 && Price < 1000)
+            {
+                return "$$$";
+            }
+            else if (Price > 100 && Price < 500)
+            {
+                return "$$";
+            }
+            else
+            {
+                return "$";
+            }
+        }
+    }
+}
